1cc0508aa509788c967ec8c6753b7a31296e7286
joniyed.bhuiyan <joniyed.bhuiyan@gmail.com>
2022-01-28 16:50:26 +0600

    issue fixed

--- Files/ArraySlice.go
+++ Files/ArraySlice.go
@@ -3,12 +3,18 @@ package Files
 import "fmt"
 
 func ArraySlices() {
+
+	//array
 	var ages []int = []int{23, 32, 55}
 	var age = ages[0]
 	age = 333
 	fmt.Println(age)
+
+	//slice
 	var slice = ages[1:2]
 	fmt.Println("Array: ", ages, "\nLen: ", len(ages), "\nArray[1:2]: ", slice, "\nLen: ", len(slice))
+
+	//append in array
 	ages = append(ages, 77)
 	fmt.Println(ages)
 }
